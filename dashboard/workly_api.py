c28dadcfcd60165261ea1a044fc084eaada0c2b6
Sherzod <azamovshavkat98@gmail.com>
2024-06-29 11:35:31 +0500

    Remove .env file from tracking

--- dashboard/workly_api.py
+++ dashboard/workly_api.py
@@ -9,6 +9,8 @@ BASE_URL = 'https://api.workly.uz/v1/'
 
 email = os.environ.get('EMAIL')
 password = os.environ.get('PASSWORD')
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
 
 
 def authorize_user():
@@ -17,8 +19,8 @@ def authorize_user():
     }
 
     data = {
-        'client_secret': 'a3ba75d0d34abe779532c2206245977466796690d5f89',
-        'client_id': '1b09927110097477452f75ec989771c866796690d5f86',
+        'client_secret': client_secret,
+        'client_id': client_id,
         'grant_type': 'password',
         'username': email,
         'password': password
@@ -44,8 +46,8 @@ def get_in_out_info():
 
     yesterday_date = (datetime.now() - timedelta(days=1)).date()
     today_date = datetime.now().date()
-    timed_url = url + f'?start_date={yesterday_date}&end_date={today_date}'
-    # timed_url = url + f'?start_date={today_date}&end_date={today_date}'
+    # timed_url = url + f'?start_date={yesterday_date}&end_date={today_date}'
+    timed_url = url + f'?start_date={today_date}&end_date={today_date}'
 
     response = requests.get(timed_url, headers=headers)
     if response.status_code == 200:
@@ -54,7 +56,7 @@ def get_in_out_info():
 
 
 def subtract_out_from_in(out_time, in_time):
-    time_format = "%H:%M:%S"
+    time_format = "%H:%M"
 
     out_time_dt = datetime.strptime(out_time, time_format)
     in_time_dt = datetime.strptime(in_time, time_format)
@@ -67,9 +69,8 @@ def subtract_out_from_in(out_time, in_time):
     total_seconds = int(time_difference.total_seconds())
     hours = total_seconds // 3600
     minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60
 
-    result = f"{hours:02}:{minutes:02}:{seconds:02}"
+    result = f"{hours:02}:{minutes:02}"
 
     return result
 
@@ -107,14 +108,19 @@ def by_users():
                     'out': ''
                 }
             if 'in' == user['event_name']:
-                ctx[user_name]['in'] = user['event_time']
+                time = user['event_time'].split(':')
+                ctx[user_name]['in'] = f'{time[0]}:{time[1]}'
             elif 'out' == user['event_name']:
-                ctx[user_name]['out'] = user['event_time']
+                time_ = user['event_time'].split(':')
+                ctx[user_name]['out'] = f'{time_[0]}:{time_[1]}'
 
     for user, val in ctx.items():
         if val['in'] and val['out']:
             val.update({'all': subtract_out_from_in(val['out'], val['in'])})
+        elif val['in']:
+            current_time = datetime.now().strftime("%H:%M")
+            val.update({'all': subtract_out_from_in(current_time, val['in'])})
         else:
-            val.update({'all': '00:00:00'})
+            val.update({'all': '00:00'})
 
     return ctx
